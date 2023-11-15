c45a22e1d45fdc102f2e3cccb639dc885e7f11d6
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2023-11-14 18:14:43 -0600

    added comedy genre

--- src/schemas/mongoose/movie.schema.js
+++ src/schemas/mongoose/movie.schema.js
@@ -1,67 +1,71 @@
 import mongoose from "mongoose";
 
-const MovieSchema = new mongoose.Schema({
+const MovieSchema = new mongoose.Schema(
+  {
     id: {
-        type: String,
-
+      type: String,
     },
     title: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 255
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 255,
     },
     year: {
-        type: Number,
-        required: true,
-        min: 1888,
-        max: 2024
+      type: Number,
+      required: true,
+      min: 1888,
+      max: 2024,
     },
     director: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 255
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 255,
     },
     duration: {
-        type: Number,
-        required: true,
-        min: 1,
-        max: 500
+      type: Number,
+      required: true,
+      min: 1,
+      max: 500,
     },
     genre: {
-        type: [String],
-        required: true,
-        enum: ['Drama',
-            'Action',
-            'Crime',
-            'Adventure',
-            'Sci-Fi',
-            'Romance',
-            'Animation',
-            'Biography',
-            'Fantasy']
+      type: [String],
+      required: true,
+      enum: [
+        "Drama",
+        "Action",
+        "Crime",
+        "Adventure",
+        "Sci-Fi",
+        "Romance",
+        "Animation",
+        "Biography",
+        "Fantasy",
+        "Comedy",
+      ],
     },
     poster: {
-        type: String,
-        required: true,
-        url: true
+      type: String,
+      required: true,
+      url: true,
     },
     rate: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 10
+      type: Number,
+      required: true,
+      min: 0,
+      max: 10,
     },
     synopsis: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 1000
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 1000,
     },
-},
-    {
-        timestamps: true
-    })
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default mongoose.model('Movie', MovieSchema)
+export default mongoose.model("Movie", MovieSchema);
--- src/schemas/movies.schema.js
+++ src/schemas/movies.schema.js
@@ -1,30 +1,33 @@
-import z from "zod"
+import z from "zod";
 
 const movieSchema = z.object({
-    title: z.string().min(1).max(255),
-    year: z.number().int().min(1888).max(2024),
-    director: z.string().min(1).max(255),
-    duration: z.number().int().min(1).max(500),
-    genre: z.array(
-        z.enum(['Drama',
-            'Action',
-            'Crime',
-            'Adventure',
-            'Sci-Fi',
-            'Romance',
-            'Animation',
-            'Biography',
-            'Fantasy'])
-    ),
-    poster: z.string().url(),
-    rate: z.number().min(0).max(10),
-    synopsis: z.string().min(1).max(1000),
-})
+  title: z.string().min(1).max(255),
+  year: z.number().int().min(1888).max(2024),
+  director: z.string().min(1).max(255),
+  duration: z.number().int().min(1).max(500),
+  genre: z.array(
+    z.enum([
+      "Drama",
+      "Action",
+      "Crime",
+      "Adventure",
+      "Sci-Fi",
+      "Romance",
+      "Animation",
+      "Biography",
+      "Fantasy",
+      "Comedy",
+    ])
+  ),
+  poster: z.string().url(),
+  rate: z.number().min(0).max(10),
+  synopsis: z.string().min(1).max(1000),
+});
 
 export function validateSchema(object) {
-    return movieSchema.safeParse(object)
+  return movieSchema.safeParse(object);
 }
 
 export function validatePartialSchema(object) {
-    return movieSchema.partial().safeParse(object)
+  return movieSchema.partial().safeParse(object);
 }
